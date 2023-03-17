408113d3e1dd3c154a0c441e8b025162ca563497
Jay Cox <jay.cox@aa.com>
2023-03-17 11:52:11 -0500

    IT WORKS!!!

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import random
 import typing as t
+import math
 
-challenge_input = r"/\.|__/\."
+challenge_input_lines = []
+with open("input.txt") as f:
+    while s := f.readline():
+        challenge_input_lines.append(s.strip())
 
-direction = 0
+inputs = []
+for i in [challenge_input_lines[i:i + 2] for i in range(0, len(challenge_input_lines), 2)]:
+    input_line = i[0].split("=")[1]
+    res = i[1].split("=")[1].replace("%", "")
+    inputs.append((input_line, res))
+
+
+def main():
+    for input, res in inputs:
+        print(math.trunc(challenge(input) * 100), res)
+
+# --------------------------------
+
+def clamp(num):
+    return max(min(num, 1), -1)
 
 acceleration_map: dict[str, t.Callable[[int], list[int]]] = {
-    "\\": lambda x: [1],
-    "/": lambda x: [-1],
+    "\\": lambda x: [clamp(x + 1)],
+    "/": lambda x: [clamp(x + (-1))],
     "_": lambda x: [x],
     "|": lambda x: [0] if abs(x) > 0 else [1, -1],
     ".": lambda x: [-99]
@@ -15,8 +33,7 @@ acceleration_map: dict[str, t.Callable[[int], list[int]]] = {
 
 probability_list: list[int | float] = []
 
-
-def main():
+def challenge(challenge_input) -> int:
     final_prob = []
     for position, char in enumerate(challenge_input):
         program_counter = position
@@ -28,20 +45,20 @@ def main():
         for accel in init_accel:
             program_counter = position + accel
 
-            accel = does_hit_hole(accel, program_counter)
+            accel = does_hit_hole(accel, program_counter, challenge_input)
             if accel == -99:
-                print(f"Position: {position}: '{char}' found a hole")
+                #print(f"Position: {position}: '{char}' found a hole")
                 prob.append(1)
             else:
-                print(f"Position: {position}: '{char}' did not find a hole")
+                #print(f"Position: {position}: '{char}' did not find a hole")
                 prob.append(0)
 
         final_prob.append(sum(prob) / len(init_accel))
 
-    print(final_prob)
+    return sum(final_prob)/len(challenge_input)
 
 
-def does_hit_hole(accel: int, program_counter: int):
+def does_hit_hole(accel: int, program_counter: int, challenge_input: str):
     while accel != 0 and accel != -99:
         if program_counter < 0 or program_counter >= len(challenge_input):
             accel = 0
