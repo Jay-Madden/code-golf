8feb71259d09e9411a38999bbcd6a78bf601c395
Jay Cox <jay.cox@aa.com>
2023-03-17 11:15:09 -0500

    working prob commit

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
 import random
+import typing as t
 
 challenge_input = r"/\.|__/\."
 
 direction = 0
 
-acceleration_map = {
-    "\\": lambda x: [x + 1],
-    "/": lambda x: [x + (-1)],
-    "_": lambda x: [x + 0],
-    #"|": lambda x: [0] if abs(x) > 0 else [1, -1],
-    "|": lambda x: [0] if abs(x) > 0 else [-1],
+acceleration_map: dict[str, t.Callable[[int], list[int]]] = {
+    "\\": lambda x: [1],
+    "/": lambda x: [-1],
+    "_": lambda x: [x],
+    "|": lambda x: [0] if abs(x) > 0 else [1, -1],
     ".": lambda x: [-99]
 }
 
 probability_list: list[int | float] = []
-for position, char in enumerate(challenge_input):
-    program_counter = position
 
-    accel = None
+
+def main():
+    final_prob = []
+    for position, char in enumerate(challenge_input):
+        program_counter = position
+
+        accel = 0
+        init_accel = acceleration_map[challenge_input[program_counter]](accel)
+
+        prob = []
+        for accel in init_accel:
+            program_counter = position + accel
+
+            accel = does_hit_hole(accel, program_counter)
+            if accel == -99:
+                print(f"Position: {position}: '{char}' found a hole")
+                prob.append(1)
+            else:
+                print(f"Position: {position}: '{char}' did not find a hole")
+                prob.append(0)
+
+        final_prob.append(sum(prob) / len(init_accel))
+
+    print(final_prob)
+
+
+def does_hit_hole(accel: int, program_counter: int):
     while accel != 0 and accel != -99:
+        if program_counter < 0 or program_counter >= len(challenge_input):
+            accel = 0
+            break
+
         try:
             if accel:
                 accel_temp = acceleration_map[challenge_input[program_counter]](accel)[0]
@@ -32,23 +60,15 @@ for position, char in enumerate(challenge_input):
             accel = -99
             break
 
-        if accel:
-            accel += accel_temp
-        else:
-            accel = accel_temp
+        if accel_temp == 0:
+            accel = 0
+            break
+
+        accel = accel_temp
         program_counter += accel
 
-        if program_counter < 0 or program_counter >= len(challenge_input):
-            accel = 0
-
-    if accel == -99:
-        print(f"Position: {position}: '{char}' found a hole")
-    else:
-        print(f"Position: {position}: '{char}' has an acceleration of: {accel}")
+    return accel
 
 
-
-
-
-
-
+if __name__ == "__main__":
+    main()
